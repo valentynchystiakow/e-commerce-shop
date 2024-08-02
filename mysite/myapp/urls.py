00235bab4c78d00feeda236af197f87ee1c77ba9
Valentyn Chystiakov <chystiakowvalentyn@gmail.com>
2024-08-02 12:56:19 +0300

    logo & tailwind

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -9,9 +9,9 @@ app_name = "myapp"
 # myapp url pathes
 urlpatterns = [
     # http://127.0.0.1:8000/myapp
-    path('', index),
+    path('', index, name='index'),
     # path for every product - by it's id
-    # http://127.0.0.1:8000/myapp
+    # http://127.0.0.1:8000/myapp/2
     path('<int:my_id>/', indexItem, name='detail'),
 
 ]
